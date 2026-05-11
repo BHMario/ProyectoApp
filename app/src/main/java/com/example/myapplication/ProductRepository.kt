b1cfcee9cc068875cb6dd42f89d91deda531a5f7
Marco <marcocc0905006@gmail.com>
2026-05-12 01:14:53 +0200

    style: Img

--- app/src/main/java/com/example/myapplication/ProductRepository.kt
+++ app/src/main/java/com/example/myapplication/ProductRepository.kt
@@ -2,14 +2,14 @@ package com.example.myapplication
 
 object ProductRepository {
     val products: MutableList<Product> = mutableListOf(
-        Product(1, "Manga Jujutsu Kaisen Vol 1", 12.99, "Manga", "Primer volumen de la popular serie Jujutsu Kaisen"),
-        Product(2, "Figura Akatsuki", 24.99, "Figuras", "Figura articulada de personaje Akatsuki"),
-        Product(3, "Camiseta Naruto", 18.99, "Merchandising", "Camiseta 100% algodón con diseño de Naruto"),
-        Product(4, "Manga One Piece Vol 5", 13.99, "Manga", "Quinto volumen de la saga One Piece"),
-        Product(5, "Figura Demon Slayer", 29.99, "Figuras", "Figura premium de Tanjiro en acción"),
-        Product(6, "Mochila Anime", 32.99, "Merchandising", "Mochila con diseños exclusivos anime"),
-        Product(7, "Manga My Hero Vol 3", 12.99, "Manga", "Tercer volumen de My Hero Academia"),
-        Product(8, "Gorro Sailor Moon", 16.99, "Merchandising", "Gorro de invierno con logo Sailor Moon")
+        Product(1, "Manga Jujutsu Kaisen Vol 1", 12.99, "Manga", "Primer volumen de la popular serie Jujutsu Kaisen", "assets://images/jujutsu_kaisen_vol1.png"),
+        Product(2, "Figura Akatsuki", 24.99, "Figuras", "Figura articulada de personaje Akatsuki", "assets://images/figura_akatsuki.png"),
+        Product(3, "Camiseta Naruto", 18.99, "Merchandising", "Camiseta 100% algodón con diseño de Naruto", "assets://images/camiseta_naruto.png"),
+        Product(4, "Manga One Piece Vol 5", 13.99, "Manga", "Quinto volumen de la saga One Piece", "assets://images/one_piece_vol5.png"),
+        Product(5, "Figura Demon Slayer", 29.99, "Figuras", "Figura premium de Tanjiro en acción", "assets://images/figura_demon_slayer.png"),
+        Product(6, "Mochila Anime", 32.99, "Merchandising", "Mochila con diseños exclusivos anime", "assets://images/mochila_anime.png"),
+        Product(7, "Manga My Hero Vol 3", 12.99, "Manga", "Tercer volumen de My Hero Academia", "assets://images/my_hero_vol3.png"),
+        Product(8, "Gorro Sailor Moon", 16.99, "Merchandising", "Gorro de invierno con logo Sailor Moon", "assets://images/gorro_sailor_moon.png")
     )
 
     fun addProduct(product: Product) {
